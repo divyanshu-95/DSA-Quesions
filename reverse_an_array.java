36ec07aace86dbfef019d8230dde2ab58f9cf744
Divyanshu Varshney <51895957+divyanshu-95@users.noreply.github.com>
2022-05-08 19:41:00 +0530

    Create reverse_an_array.java

---
+++ reverse_an_array.java
@@ -0,0 +1,9 @@
+public class Main
+{
+	public static void main(String[] args) {
+        int arr[]={5,9,3,7,19,9};
+        for(int i=arr.length-1;i>=0;i--){
+            System.out.println(arr[i]);
+        }
+	}
+}
