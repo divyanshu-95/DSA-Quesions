e29cf382e48854fefe3ee9ac644642b129721638
Divyanshu Varshney <51895957+divyanshu-95@users.noreply.github.com>
2022-05-08 19:37:59 +0530

    Create find_max_min_array.java

---
+++ find_max_min_array.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+        int arr[]={5,9,3,7,19,9};
+        Arrays.sort(arr);
+        int min=arr[0];
+        int max=(arr[arr.length-1]);
+        System.out.println(min);
+        System.out.println(max);
+	}
+}
